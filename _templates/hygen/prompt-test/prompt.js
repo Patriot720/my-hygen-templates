8b74e6079d1923d2cda0d145fc60c396d6bb76b1
Patriot720 <cerkin-3@yandex.ru>
2021-04-03 16:27:54 +0300

    initial

---
+++ _templates/hygen/prompt-test/prompt.js
@@ -0,0 +1,12 @@
+module.exports = [
+    {
+        type: 'input',
+        name: 'message',
+        message: "What's your message?"
+    },
+    {
+        type: 'list',
+        name: 'keywords',
+        message: 'List things:',
+    }
+]
